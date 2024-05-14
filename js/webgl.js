17ef40ea5212b1110c915c1949787f439d43a909
silvericarus <agonzalezrosa@hotmail.com>
2024-05-14 03:04:54 +0200

    Multiple temporary fixes

--- js/webgl.js
+++ js/webgl.js
@@ -4,22 +4,36 @@ var canvasX;
 var canvasY;
 var tileX, tileY;
 var board 
-var rows = 150;
-var cols = 300;
-var randomSizeOfForest = Math.floor(Math.random() * 300);
+var rows;
+var cols;
+var randomSizeOfForest = Math.floor(Math.random() * 50);
 const forest = new Forest();
 
 function treeGeneration() {
+	let treesGenerated = 0;
+	let lastPosition = [];
 	for(var i = 0; i < randomSizeOfForest; i++){
-		for(var j = 0; j < rows; j++){
-			for(var k = 0; k < cols; k++){
-				const trueCheck = Math.random();
-				if(trueCheck == 1 && board[j][k] === 'L'){
-					board[j][k] = new Tree("Oak", j, k, tileX, tileY);
+		for(var j = 0; j < canvasY; j++){
+			for(var k = 0; k < canvasX; k++){
+				if(treesGenerated >= randomSizeOfForest){
+					break;
+				}
+				//TODO - Fix the random generation of trees
+				const x = Math.floor(Math.random() * cols) / 6;
+                const y = Math.floor(Math.random() * rows) / 6;
+				if((Math.abs(lastPosition[0] - x) > 4 &&
+					Math.abs(lastPosition[1] - y) > 4) ||
+					treesGenerated === 0){
+					let tree = new Tree("Oak", x, y, tileX, tileY);
+					forest.addTree(tree);
+					board[x][y] = 'T';
+					treesGenerated++;
+					lastPosition = [x, y];
 				}
 			}
 		}
 	}
+	console.table(forest.trees);
 }
 
 function start() {
@@ -49,10 +63,12 @@ function startGL() {
 
 function initGL(canvas) {
 	cntxt = canvas.getContext("2d");
-	canvasWidth = canvas.clientWidth;
-	canvasHeight = canvas.clientHeight;
-	canvasX = canvasWidth * 2;
-	canvasY = canvasHeight * 2;
+	canvasWidth = 375;
+	canvasHeight = 187;
+	canvasX = canvasWidth * 2.5;
+	canvasY = canvasHeight * 19;
+	rows = 150;
+	cols = 300;
 	tileX = Math.floor(canvasX / rows);
 	tileY = Math.floor(canvasY / cols);
 	board = create_board(rows, cols);
